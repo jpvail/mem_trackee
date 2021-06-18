d99911238f69db65cea63cad13b9fd040c31db5b
Jason Vail <jasonvail@Jasons-MacBook-Pro-2.local>
2021-06-18 15:53:00 -0400

    second commit

---
+++ client/client.js
@@ -0,0 +1,18 @@
+
+const fetch = require("node-fetch"); 
+
+const url = "http://localhost:3000/"; 
+
+//fetch 10 web pages in the first 10 seconds 
+for(let i = 0; i < 10; ++i){
+    fetch(url);
+}
+
+//fetch 10 web pages every following 10 seconds 
+setInterval( async () => {
+    for(let i = 0; i < 10; ++i){
+        await fetch(url);
+    }
+}, 10000);  
+
+
